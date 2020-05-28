2bb31917da407626deddc82f1e72f18e9e5ae7c1
Marta Rudnik <marta.rudnik@intive.com>
2020-05-28 15:09:30 +0200

    MR: First step of app

---
+++ MRTwitter/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace MRTwitter.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.12.0.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/unobtrusive").Include(
+                    "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/html5shiv").Include(
+                      "~/Scripts/html5shiv.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/screen.css"));
+        }
+    }
+}
--- MRTwitter/Global.asax.cs
+++ MRTwitter/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MRTwitter.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MRTwitter
@@ -13,6 +15,7 @@ namespace MRTwitter
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
