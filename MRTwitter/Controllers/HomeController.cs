3fd0afc9f7fa8629e54ef53099abbabd70df9daa
Marta Rudnik <marta.rudnik@intive.com>
2020-05-28 18:45:05 +0200

    MR: clean code

--- MRTwitter/Controllers/HomeController.cs
+++ MRTwitter/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace MRTwitter.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var asd = System.Configuration.ConfigurationManager.AppSettings["emailAddress"];
             return View();
         }
     }
